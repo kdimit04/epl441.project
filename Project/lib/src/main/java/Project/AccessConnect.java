ec0fbb06bc0a12f6ae0aad23e3234ee77894b253
tdoulg01 <49614554+tdoulg01@users.noreply.github.com>
2022-05-09 21:59:08 +0300

    needs work done

---
+++ Project/lib/src/main/java/Project/AccessConnect.java
@@ -0,0 +1,21 @@
+package Project;
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class AccessConnect {
+ 
+    public static void main(String[] args) {
+      
+          try{
+              Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");//Loading Driver
+              Connection connection= DriverManager.getConnection("jdbc:ucanaccess://C:\\Users\\doulg\\Desktop\\Database11.accdb");//Establishing Connection
+              System.out.println("Connected Successfully");
+ 
+          }catch(Exception e){
+              System.out.println("Error in connection");
+ 
+          }
+    }
+    
+}
+
